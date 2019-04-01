bb32e96e8ffff7dc5415f86db8f208003660a267
EC2 Default User <ec2-user@ip-172-31-75-93.ec2.internal>
2019-04-01 06:23:51 +0000

    project_category model

--- app/models/university.rb
+++ app/models/university.rb
@@ -2,4 +2,6 @@ class University < ApplicationRecord
   validates :name, presence: true,
                    length: { maximum: 50 },
                    uniqueness: true
+  
+  has_many :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190401055206) do
+ActiveRecord::Schema.define(version: 20190401062109) do
+
+  create_table "project_categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "universities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"

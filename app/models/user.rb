9d31e7c621716fdd78402680e72b7c4a70e2ebb5
EC2 Default User <ec2-user@ip-172-31-75-93.ec2.internal>
2019-04-01 09:17:05 +0000

    pagination

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,19 @@ class User < ApplicationRecord
   has_many :followings, through: :user_relationships, source: :follow
   has_many :reverse_of_user_relationships, class_name: 'User_relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverse_of_user_relationships, source: :user
+  
+  def follow(other_user)
+    unless self == other_user
+      self.user_relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+  
+  def unfollow(other_user)
+    user_relationship = self.user_relationships.find_by(follow_id: other_user.id)
+    user_relationship.destriy if user_relationship
+  end
+  
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
 end

d4b58671b1550016d7994c94c5e7f49334285d0f
EC2 Default User <ec2-user@ip-172-31-75-93.ec2.internal>
2019-04-22 16:39:13 +0000

    change set_search in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show, :edit, :update, :destroy]
-  before_action :set_search
+  before_action :set_search, only: [:index, :show, :edit, :followings, :followers, :likes, :participatings]
   
   
   def index

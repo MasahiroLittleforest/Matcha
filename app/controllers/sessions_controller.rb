4e8776b425864b7024b4f4ec9771a5ac20525aad
github MasahiroLittleforest <masahiro.littleforest@gmail.com>
2019-05-28 19:06:00 +0900

    added a sample login button to toppage

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,4 +19,16 @@ class SessionsController < ApplicationController
     flash[:success] = 'ログアウトしました。'
     redirect_to root_url
   end
+
+  def sample_login
+    email = "matcha@example.com"
+    password = "matchamatcha"
+    if login(email, password)
+      flash[:success] = 'ログインしました。'
+      redirect_to projects_url
+    else
+      flash.now[:danger] = 'ログインできませんでした。'
+      render 'new'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  get 'sample_login', to: 'sessions#sample_login'
   
   get 'signup', to: 'users#new'
   resources :users do
